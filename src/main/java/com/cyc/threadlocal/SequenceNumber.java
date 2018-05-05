bddbcc224e2608101414b5b7ad1751baf67b79c5
cyc_eason@126.com <cyc_eason@126.com>
2018-05-05 22:03:24 +0800

    0.0

--- src/main/java/com/cyc/threadlocal/SequenceNumber.java
+++ src/main/java/com/cyc/threadlocal/SequenceNumber.java
@@ -34,6 +34,7 @@ class TestClient implements Runnable {
         this.sn = sn;
     }
 
+    @Override
     public void run() {
         for (int i = 0; i < 3; i++) {
             System.out.println("当前线程 : " + Thread.currentThread() + " ;sn对象 :  " + sn + " ;sn字段 ：" + sn.seqNum + " : " + sn.getNextNum());
