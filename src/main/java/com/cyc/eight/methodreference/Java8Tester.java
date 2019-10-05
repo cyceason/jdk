217df4f313795a296d5a7c85035aa8a2a2006ebe
CYC <cyc_eason@126.com>
2019-10-05 15:16:34 +0800

    更新

--- src/main/java/com/cyc/eight/methodreference/Java8Tester.java
+++ src/main/java/com/cyc/eight/methodreference/Java8Tester.java
@@ -20,17 +20,17 @@ public class Java8Tester {
 
 
     public static void test2() {
-        // 构造器引用：它的语法是Class::new，或者更一般的Class< T >::new实例如下：
+        // 构造器引用：它的语法是Class::new ：
         final Car car = Car.create(Car::new);
         final List<Car> cars = Arrays.asList(car);
 
         // 静态方法引用：它的语法是Class::static_method，实例如下：
         cars.forEach(Car::collide);
 
-        // 特定类的任意对象的方法引用：它的语法是Class::method实例如下：
+        // 引用某个类型的任意对象的实例方法：它的语法是Class::method实例如下：
         cars.forEach(Car::repair);
 
-        // 特定对象的方法引用：它的语法是instance::method实例如下：
+        // 引用某个对象的实例方法：它的语法是instance::method实例如下：
         final Car police = Car.create(Car::new);
         cars.forEach(police::follow);
     }
